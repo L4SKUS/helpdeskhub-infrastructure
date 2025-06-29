056872582e64320f1dafc4c1487375282f5aaa8b
Błażej Laskus <01169315@pw.edu.pl>
2025-06-30 00:29:06 +0200

    Remove backend s3 from state

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "website_url" {
-  description = "URL of the React app hosted on S3"
-  value       = aws_s3_bucket_website_configuration.helpdeskhub_app_website.website_endpoint
-}
-
-output "cloudfront_url" {
-  value = aws_cloudfront_distribution.helpdeskhub_app_distribution.domain_name
-}
